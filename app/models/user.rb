6b1dd60086bc673ac6a2beb7a0eabf0598b587c3
yamaday0u <35hallelujah00@gmail.com>
2021-01-09 23:37:20 +0900

    Update validations foruser model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
+  validates :first_name, presence: true,
+                 format: {
+                   with: /\A[^ -~｡-ﾟ]+\z/,
+                   message: "には全角で入力して下さい"
+                 }
+  validates :family_name, presence: true,
+                  format: {
+                    with: /\A[^ -~｡-ﾟ]+\z/,
+                    message: "には全角で入力して下さい"
+                  }
+  validates :first_name_kana, presence: true,
+                 format: {
+                   with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                   message: "には全角カタカナのみで入力して下さい"
+                 }
+  validates :family_name_kana, presence: true,
+                  format: {
+                    with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
+                    message: "には全角カタカナのみで入力して下さい"
+                  }
   validates :birthday, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
