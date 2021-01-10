af6cf25e7c2046f212fe3245c8e7edd96db78558
yamaday0u <35hallelujah00@gmail.com>
2021-01-10 12:31:21 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe User, type: :model do
 
   describe '新規登録/ユーザー情報' do
     context '新規登録できるとき' do
+      it "全ての項目が存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上の半角英数字であれば登録できる" do
+        @user.password = "123abc"
+        @user.password_confirmation = @user.password
+        expect(@user).to be_valid
+      end
     end
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
