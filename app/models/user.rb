efdebf70008f688204ba8e785a2f713433226d76
yamaday0u <35hallelujah00@gmail.com>
2021-01-10 13:02:31 +0900

    Refactor validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true,
-                         format: {
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: {
                            with: /\A[^ -~｡-ﾟ]+\z/,
                            message: 'には全角で入力して下さい'
                          }
-  validates :family_name, presence: true,
-                          format: {
+    validates :family_name, format: {
                             with: /\A[^ -~｡-ﾟ]+\z/,
                             message: 'には全角で入力して下さい'
                           }
-  validates :first_name_kana, presence: true,
-                              format: {
+    validates :first_name_kana, format: {
                                 with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
                                 message: 'には全角カタカナのみで入力して下さい'
                               }
-  validates :family_name_kana, presence: true,
-                               format: {
+    validates :family_name_kana,  format: {
                                  with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,
                                  message: 'には全角カタカナのみで入力して下さい'
                                }
-  validates :birthday, presence: true
+    validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字と数字の両方を含めて設定してください'
+  end
+
 end
