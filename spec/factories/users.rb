a5cc1d89bea29a485a4925673e2ca2419c624706
yamaday0u <35hallelujah00@gmail.com>
2021-01-10 12:22:03 +0900

    Update instance for password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 5) }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { "1a" + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     family_name { Faker::Name.last_name }
     first_name { Faker::Name.first_name }
