10b5417d6bb9ebd815fe29fdb1b152231939bc60
yamaday0u <35hallelujah00@gmail.com>
2021-01-14 23:01:01 +0900

    Create pull down menu for category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+# 以下に他のカラムのアソシエーションを追加する
+
   has_one_attached :image
 end
