d620e409690ce066c782b88f2ccdfb4152b93174
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 00:01:49 +0900

    Create pull down menu for shipping area

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
 # 以下に他のカラムのアソシエーションを追加する
   belongs_to_active_hash :item_status
   belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :shipping_area
 
   has_one_attached :image
 end
