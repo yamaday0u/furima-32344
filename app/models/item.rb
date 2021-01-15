b9b66a6e6efa463e3e641fbc6b173a767bb8dc98
yamaday0u <35hallelujah00@gmail.com>
2021-01-15 23:44:08 +0900

    Create pull down menu for item status

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
 # 以下に他のカラムのアソシエーションを追加する
+  belongs_to_active_hash :item_status
 
   has_one_attached :image
 end
