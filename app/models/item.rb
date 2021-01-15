7e91f99f306a6421beeb7c39562b4fa8b8928684
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 00:06:23 +0900

    Create pull down menu for days to ship

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_status
   belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :days_to_ship
 
   has_one_attached :image
 end
