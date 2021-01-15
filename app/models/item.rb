9874e34989c2945c17a6bcb4a84a8bf4e317fb77
yamaday0u <35hallelujah00@gmail.com>
2021-01-15 23:52:29 +0900

    Create pull down menu for deilvery fee

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
 # 以下に他のカラムのアソシエーションを追加する
   belongs_to_active_hash :item_status
+  belongs_to_active_hash :delivery_fee
 
   has_one_attached :image
 end
