a9b30a24ea3b121d8d09130d4b1e4923b1931ef0
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 21:39:47 +0900

    Implement create action for exhibiting item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,14 +9,27 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      return redirect_to  root_path
+    else
+      render "new"
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(
-      :name, :explantion, :image, :category_id, :item_status_id,
-      :delivery_fee_id, :shipping_area_id, :days_to_ship_id, :price
+      :name,
+      :explanation,
+      :image,
+      :category_id,
+      :item_status_id,
+      :delivery_fee_id,
+      :shipping_area_id,
+      :days_to_ship_id,
+      :price
     ).merge(user_id: current_user.id)
   end
 end
