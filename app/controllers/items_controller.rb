7f71b160e56248b7819ea9debfe814924d0a96aa
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 13:54:49 +0900

    Create root to page for exhibiting item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
     
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name, :explantion, :image, :category_id, :item_status_id,
+      :delivery_fee_id, :shipping_area_id, :days_to_ship_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
