9db2a069139297123fb6e0fe0c213d8ad6377559
yamaday0u <35hallelujah00@gmail.com>
2021-01-16 09:27:19 +0900

    Add validations for items table

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  belogs_to :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
 # 以下に他のカラムのアソシエーションを追加する
@@ -8,4 +10,22 @@ class Item < ApplicationRecord
   belongs_to_active_hash :days_to_ship
 
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :explantion
+    validates :image
+    with_options numericality: {ohter_than: 1} do
+      validates :category
+      validates :item_status
+      validates :delivery_fee
+      validates :shipping_area
+      validates :days_to_ship
+    end
+    validates :price、format: {
+      with: /\A\d+\z/, message: "is invalid. Input half-width characters.",
+      with: /[\A3.{2,}]-[9{,7}]/, message: "is "
+    }
+  end
+  
 end
