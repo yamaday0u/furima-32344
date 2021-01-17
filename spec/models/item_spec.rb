0390e9220a286d57e941f549090c3d02cf69ac89
yamaday0u <35hallelujah00@gmail.com>
2021-01-17 21:15:56 +0900

    Add test code

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,25 +32,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
+      it 'category_idが未選択では出品できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it 'item_status_idが1では出品できない' do
         @item.item_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Item status must be other than 1')
+      end
+      it 'item_status_idが未選択では出品できない' do
+        @item.item_status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
       end
       it 'delivery_feeが1では出品できない' do
         @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
       end
+      it 'delivery_fee_idが未選択では出品できない' do
+        @item.delivery_fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
       it 'shipping_area_idが1では出品できない' do
         @item.shipping_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping area must be other than 1')
       end
+      it 'shipping_area_idが未選択では出品できない' do
+        @item.shipping_area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area can't be blank")
+      end
       it 'days_to_ship_idが1では出品できない' do
         @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
+      end
+      it 'days_to_ship_idが未選択では出品できない' do
+        @item.days_to_ship_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
       it 'priceが空では出品できない' do
         @item.price = nil
