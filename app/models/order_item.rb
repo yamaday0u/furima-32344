f58bb0fd4d8f083e0fe136394f5e5d09ee467c94
yamaday0u <35hallelujah00@gmail.com>
2021-01-28 20:11:11 +0900

    Update validation in OrderItem class

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,14 +3,11 @@ class OrderItem
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id
   
   with_options presence: true do
-    validates :user_id, forign_key: true
-    validates :item_id, forign_key: true
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :address
     validates :phone_number, numericality: { only_integer: true, message: "Input only number"}
-    validates :purchase_id, forign_key: true
   end
 
   def save
