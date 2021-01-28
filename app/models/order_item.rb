89f346f259397078ba4e45229a703b0a946b4f69
yamaday0u <35hallelujah00@gmail.com>
2021-01-28 23:25:43 +0900

    Update OrderItem class for create method of Purchase & Delivery

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,22 +4,22 @@ class OrderItem
   
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :address
     validates :phone_number, numericality: { only_integer: true, message: "Input only number"}
   end
 
   def save
-    Purchase.create(user_id: current_user.id, item_id: item_id)
-    Delivery.create(
-      :postal_code,
-      :prefecture_id,
-      :city, 
-      :address, 
-      :building, 
-      :phone_number,
-      :purchase_id
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    delivery = Delivery.create(
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city, 
+      address: address, 
+      building: building, 
+      phone_number: phone_number,
+      purchase_id: purchase.id
     )
   end
 end
