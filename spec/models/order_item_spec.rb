b968c4ad577f2307934bed8a18b241487ef6eb7b
yamaday0u <35hallelujah00@gmail.com>
2021-01-31 20:36:45 +0900

    Update create instances before test

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderItem, type: :model do
   before do
-    @purchase = FactoryBot.build(:order_item)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @purchase = FactoryBot.build(:order_item, user_id: @user.id, item_id: @item.id)
   end
 
   describe '商品購入機能' do
