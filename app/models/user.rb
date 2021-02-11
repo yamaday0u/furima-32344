f4bd08aa960aea8f81d45dea30d3934797cef4fc
yamaday0u <35hallelujah00@gmail.com>
2021-02-11 15:18:50 +0900

    User has one attached avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   with_options presence: true do
     validates :nickname
     validates :first_name, format: {
