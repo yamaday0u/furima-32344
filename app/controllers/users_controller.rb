af978fcdccf7519571f00831840de39805667a4a
yamaday0u <35hallelujah00@gmail.com>
2021-02-13 15:10:44 +0900

    Add edit action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id]).order('created_at DESC')
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
