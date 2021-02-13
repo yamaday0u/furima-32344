47af69d5580e5a47da31ceee35f9f45c141a6807
yamaday0u <35hallelujah00@gmail.com>
2021-02-14 00:39:29 +0900

    Implement function for edit user informations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -38,10 +38,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
+  # call "update without current password"
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    resource.update_without_current_password(params)# "update_without_current_password(params)" method is in "user.rb"
+  end
+
+  def after_update_path_for(resource)
+    user_path(@user.id)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -50,9 +55,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,
+      :user_iamge,
+      :first_name,
+      :family_name,
+      :first_name_kana,
+      :family_name_kana,
+      :birthday,
+      :avatar,
+      :introduction])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,24 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  # allow users to update their accounts without passwords
+  def update_without_current_password(params)
+    self.attributes = params
+    self.save
+  end
+
+  def validate_specific_columns(columns)
+    errors = 0
+    columns.valid?
+    # binding.pry
+    columns.errors.full_messages.each do |error|
+      unless error.include?("Password")
+        errors += 1
+      end
+    end
+    return errors
+  end
+
   with_options presence: true do
     validates :nickname
     validates :first_name, format: {
@@ -27,6 +45,6 @@ class User < ApplicationRecord
     validates :birthday
 
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字と数字の両方を含めて設定してください'
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角の英字と数字の両方を含めて設定してください', on: :create # validate only users/registrations#create
   end
 end
