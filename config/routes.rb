5d2ff223a6ba85765f3d6152de5748a448f64d70
yamaday0u <35hallelujah00@gmail.com>
2021-02-13 17:33:51 +0900

    Update routes for users/registrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :purchases, only: [:index, :create]
   end
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
