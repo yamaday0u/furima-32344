7c2263a59ba7b8500902d6729c917d3fb5d7a84a
yamaday0u <35hallelujah00@gmail.com>
2021-02-16 23:54:16 +0900

    Defined association user & purchase

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :purchases
   has_one_attached :avatar
 
   # allow users to update their accounts without passwords
