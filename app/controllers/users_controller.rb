a8292dc39109c19d0c0d704911241a606a3b2089
yamaday0u <35hallelujah00@gmail.com>
2021-02-16 23:56:01 +0900

    Define prototype actions to show ranking by numbers of orders

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
+
+  def index #show users ranking 
+    @users = User.all
+    @rankers = show_rankers_by_orders(@users)
+  end
+
   def show
     @user = User.find(params[:id])
     @items = Item.where(user_id: params[:id]).order('created_at DESC')
+  end
+
+  private
+  def show_rankers_by_orders(users)
+    ranking = []
+    users.each do |user|
+      order_count = {user_nickname: user.nickname, orders: user.purchases.count}
+      ranking << order_count
+    end
+    ranking = ranking.sort_by { |ranker| ranker[:orders] }.reverse
+    return ranking
   end
 end
