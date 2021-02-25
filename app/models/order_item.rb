79999b44f276710981708448e88fa994ba128e88
yamaday0u <35hallelujah00@gmail.com>
2021-02-25 22:40:53 +0900

    Update spec/models/order_item_spec.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,12 +3,12 @@ class OrderItem
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: '郵便番号はハイフン(-)を入れてください' }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input number with 10 or 11 digits' },
-                             numericality: { only_integer: true, message: 'is invalid. Input only half size number' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は半角数字(10〜11ケタ)で入力してください' },
+                             numericality: { only_integer: true, message: 'は半角数字(10〜11ケタ)で入力してください' }
     validates :user_id
     validates :item_id
     validates :token
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -22,72 +22,72 @@ RSpec.describe OrderItem, type: :model do
       it '郵便番号が空では購入できないこと' do
         @purchase.postal_code = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Postal code can't be blank")
+        expect(@purchase.errors.full_messages).to include("郵便番号 が空欄です")
       end
       it '郵便番号にハイフンがないと購入できないこと' do
         @purchase.postal_code = '1001111'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+        expect(@purchase.errors.full_messages).to include('郵便番号 郵便番号はハイフン(-)を入れてください')
       end
       it '都道府県が空では購入できないこと' do
         @purchase.prefecture_id = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@purchase.errors.full_messages).to include("都道府県 が選択されていません")
       end
       it '都道府県のid:1を選択した場合、購入できないこと' do
         @purchase.prefecture_id = 1
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include('Prefecture must be other than 1')
+        expect(@purchase.errors.full_messages).to include('都道府県 が選択されていません')
       end
       it '市区町村が空では購入できないこと' do
         @purchase.city = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("City can't be blank")
+        expect(@purchase.errors.full_messages).to include("市区町村 が空欄です")
       end
       it '番地が空では購入できないこと' do
         @purchase.address = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Address can't be blank")
+        expect(@purchase.errors.full_messages).to include("番地 が空欄です")
       end
       it '電話番号が空では購入できないこと' do
         @purchase.phone_number = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Phone number can't be blank")
+        expect(@purchase.errors.full_messages).to include("電話番号 が空欄です")
       end
       it '電話番号はハイフンが不要であること' do
         @purchase.phone_number = '090-123-567'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include('Phone number is invalid. Input only half size number')
+        expect(@purchase.errors.full_messages).to include('電話番号 は半角数字(10〜11ケタ)で入力してください')
       end
       it '電話番号は11桁以内でないと購入できないこと' do
         @purchase.phone_number = '090123456789'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include('Phone number is invalid. Input number with 10 or 11 digits')
+        expect(@purchase.errors.full_messages).to include('電話番号 は半角数字(10〜11ケタ)で入力してください')
       end
       it '電話番号は全角数字だと登録できない事購入できないこと' do
         @purchase.phone_number = '０９０１２３４５６７８'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include('Phone number is invalid. Input only half size number')
+        expect(@purchase.errors.full_messages).to include('電話番号 は半角数字(10〜11ケタ)で入力してください')
       end
       it '電話番号は9桁以下だと購入できないこと' do
         @purchase.phone_number = '090123456'
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include('Phone number is invalid. Input number with 10 or 11 digits')
+        expect(@purchase.errors.full_messages).to include('電話番号 は半角数字(10〜11ケタ)で入力してください')
       end
       it 'user_idが空だと登録できない' do
         @purchase.user_id = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("User can't be blank")
+        expect(@purchase.errors.full_messages).to include("ユーザー が空欄です")
       end
       it 'item_idが空だと登録できない' do
         @purchase.item_id = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Item can't be blank")
+        expect(@purchase.errors.full_messages).to include("商品 が空欄です")
       end
       it 'トークンが空では登録できないこと' do
         @purchase.token = ''
         @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Token can't be blank")
+        expect(@purchase.errors.full_messages).to include("クレジットカード の情報が正しくありません")
       end
     end
   end
