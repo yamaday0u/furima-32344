a078362e8e42ed39349c0ed56a7a2fc37afb23c6
yamaday0u <35hallelujah00@gmail.com>
2021-03-14 00:27:02 +0900

    Updated comment_channel.js

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -10,6 +10,10 @@ consumer.subscriptions.create("CommentChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    const html = `<p>${data.user.nickname}</p><p>${data.content.text}</p>`;
+    const messages = document.getElementById('messages');
+    const newMessage = document.getElementById('comment_text');
+    messages.insertAdjacentHTML('afterbegin', html);
+    newMessage.value='';
   }
 });
