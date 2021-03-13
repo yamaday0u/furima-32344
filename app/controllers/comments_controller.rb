6fa12a582c867a26c2c9f7bf709123dbef3b440f
yamaday0u <35hallelujah00@gmail.com>
2021-03-14 00:25:09 +0900

    Defined action for comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @user = current_user
+    if @comment.save
+      ActionCable.server.broadcast 'comment_channel', content: @comment, user: @user
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
