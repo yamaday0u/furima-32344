f7d551e450c312244fb523367bb89575c4bbab5e
yamaday0u <35hallelujah00@gmail.com>
2021-05-05 23:15:36 +0900

    Added if condition to card.js & item_price.js

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,38 +1,42 @@
-const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    // Substitute submitted form data for a variable
-    const formResult = document.getElementById("charge-form");
-    const formData = new FormData(formResult);
+if (location.pathname.match("/purchases")) {
 
-    const card = {
-      number: formData.get("order_item[number]"),
-      cvc: formData.get("order_item[cvc]"),
-      exp_month: formData.get("order_item[exp_month]"),
-      exp_year: `20${formData.get("order_item[exp_year]")}`,
-    };
-    
-    // Tokenization credit card information through PAY.JP
-    Payjp.createToken(card, (status, response) => {
-      if (status == 200) {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type='hidden'>`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
-
-      // Delete inputted information for credit card
-      document.getElementById("card-number").removeAttribute("name");
-      document.getElementById("card-exp-month").removeAttribute("name");
-      document.getElementById("card-exp-year").removeAttribute("name");
-      document.getElementById("card-cvc").removeAttribute("name");
+  const pay = () => {
+    Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+    const form = document.getElementById("charge-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
       
-      document.getElementById("charge-form").submit();
+      // Substitute submitted form data for a variable
+      const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
+  
+      const card = {
+        number: formData.get("order_item[number]"),
+        cvc: formData.get("order_item[cvc]"),
+        exp_month: formData.get("order_item[exp_month]"),
+        exp_year: `20${formData.get("order_item[exp_year]")}`,
+      };
+      
+      // Tokenization credit card information through PAY.JP
+      Payjp.createToken(card, (status, response) => {
+        if (status == 200) {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type='hidden'>`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+  
+        // Delete inputted information for credit card
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        
+        document.getElementById("charge-form").submit();
+      });
     });
-  });
-};
+  };
+  
+  window.addEventListener("load", pay)
 
-window.addEventListener("load", pay)
+}
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,16 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value
+if (location.pathname.match("/items/new")) {
 
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue*0.1);
-
-    const salesProfit = document.getElementById("profit");
-    salesProfit.innerHTML = Math.floor(inputValue) - Math.floor(inputValue*0.1)
+  window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value
+  
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+  
+      const salesProfit = document.getElementById("profit");
+      salesProfit.innerHTML = Math.floor(inputValue) - Math.floor(inputValue*0.1)
+    })
   })
-})
 
+}
